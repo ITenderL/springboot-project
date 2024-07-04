f664f74c84e3aa4b47193239a0727e1848aa4b99
yuanhewei <yuanhewei@sigo.cn>
2024-07-04 16:45:13 +0800

    feat: redis pipeline学习代码提交

--- springboot-redis/src/main/java/com/itender/redis/controller/PipelineController.java
+++ springboot-redis/src/main/java/com/itender/redis/controller/PipelineController.java
@@ -2,16 +2,19 @@ package com.itender.redis.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SessionCallback;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author yuanhewei
@@ -22,6 +25,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/pipelines")
 public class PipelineController {
+
+    @PostConstruct
+    public void init() {
+        long startTime = System.currentTimeMillis();
+        redisTemplate.executePipelined(new SessionCallback<Object>() {
+            @Override
+            public <K, V> Object execute(RedisOperations<K, V> operations) throws DataAccessException {
+                operations.hasKey((K) "");
+                return null;
+            }
+        });
+        log.info("redis初始化管道请求end，耗时：{}ms", System.currentTimeMillis() - startTime);
+    }
 
     @Resource
     private RedisTemplate<String, String> redisTemplate;
@@ -43,5 +59,32 @@ public class PipelineController {
         log.info("objects: {}", objects);
         // 获取所有 pipeline 列表
         return "success";
+    }
+
+
+    /**
+     * 批量添加
+     *
+     * @param map
+     */
+    public void batchSet(Map<String, String> map) {
+        redisTemplate.opsForValue().multiSet(map);
+    }
+
+
+    /**
+     * 批量添加 并且设置失效时间
+     *
+     * @param map
+     * @param seconds
+     */
+    public void batchSetOrExpire(Map<String, String> map, Long seconds) {
+        RedisSerializer<String> serializer = redisTemplate.getStringSerializer();
+        redisTemplate.executePipelined((RedisCallback<String>) connection -> {
+            map.forEach((key, value) ->
+                    connection.set(Objects.requireNonNull(serializer.serialize(key)), Objects.requireNonNull(serializer.serialize(value)), Expiration.seconds(seconds), RedisStringCommands.SetOption.UPSERT)
+            );
+            return null;
+        }, serializer);
     }
 }
