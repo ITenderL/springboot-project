47eae1b7811f424d67882840ca5f324d872994bc
yuanhewei <yuanhewei@sigo.cn>
2024-07-13 17:21:02 +0800

    docs:CompletableFuture使用代码

---
+++ springboot-job/src/test/java/com/itender/job/SpringbootXxljobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itender.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootXxljobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
